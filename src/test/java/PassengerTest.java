bbe71ebc10e30bcb6d68fddfe0708d1a714826f8
Chris Barclay <chris.barclay1@gmail.com>
2023-04-16 21:07:56 +0100

    Advanced Extensions Completed

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class PassengerTest {
 
@@ -21,6 +22,18 @@ public class PassengerTest {
     @Test
     public void passengerHasBags(){
         assertEquals(2, passenger.getNumberOfBags());
+    }
+
+    @Test
+    public void flightCanBeAssignedToPassenger(){
+        passenger.setFlight("AB12344");
+        assertEquals("AB12344", passenger.getFlight());
+    }
+
+    @Test
+    public void passengerCanBeGivenSeatNumber(){
+        passenger.setSeatNumber(2);
+        assertEquals(2, passenger.getSeatNumber());
     }
 
 }
